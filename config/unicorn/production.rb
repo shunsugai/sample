347a4a2a147e479436686c998401c9d9932247e0
Shun Sugai <sugaishun@gmail.com>
2013-12-30 11:44:45 +0900

    Fix pid path

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,6 +1,6 @@
 project_root_dir = File.expand_path('../../../../', __FILE__)
 worker_processes 4
 listen      "#{project_root_dir}/shared/tmp/sockets/sample.sock"
-pid         "#{project_root_dir}/tmp/pids/unicorn.pid"
+pid         "#{project_root_dir}/shared/tmp/pids/unicorn.pid"
 stdout_path "#{project_root_dir}/log/production.log"
 stderr_path "#{project_root_dir}/log/production.log"
