0c0d6a8602e13b2bf6a65cd4531818ae38b34a4e
Shun Sugai <sugaishun@gmail.com>
2013-12-30 11:48:12 +0900

    Change path of log

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -2,5 +2,5 @@ project_root_dir = File.expand_path('../../../../', __FILE__)
 worker_processes 4
 listen      "#{project_root_dir}/shared/tmp/sockets/sample.sock"
 pid         "#{project_root_dir}/shared/tmp/pids/unicorn.pid"
-stdout_path "#{project_root_dir}/log/production.log"
-stderr_path "#{project_root_dir}/log/production.log"
+stdout_path "#{project_root_dir}/shared/log/production.log"
+stderr_path "#{project_root_dir}/shared/log/production.log"
