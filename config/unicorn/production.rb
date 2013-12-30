771d5a19cf6f04d84b39ca4bf1091fa8d0c5d697
Shun Sugai <sugaishun@gmail.com>
2013-12-30 12:00:27 +0900

    Change socket location

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,6 +1,6 @@
 project_root_dir = File.expand_path('../../../../', __FILE__)
 worker_processes 4
-listen      "#{project_root_dir}/shared/tmp/sockets/sample.sock"
+listen      "/tmp/sample-rails-app.sock"
 pid         "#{project_root_dir}/shared/tmp/pids/unicorn.pid"
 stdout_path "#{project_root_dir}/shared/log/production.log"
 stderr_path "#{project_root_dir}/shared/log/production.log"
