9e016769f8307bdcf7a393ec90334f7025b0c67e
Shun Sugai <sugaishun@gmail.com>
2014-01-01 10:19:24 +0900

    Untrack config files

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,19 +1,8 @@
 set :stage, :production
 set :migration_role, 'db'
 
-# Simple Role Syntax
-# ==================
-# Supports bulk-adding hosts to roles, the primary
-# server in each group is considered to be the first
-# unless any hosts have the primary property set.
 role :all, %w{shun@www2257ue.sakura.ne.jp}
 
-# Extended Server Syntax
-# ======================
-# This can be used to drop a more detailed server
-# definition into the server list. The second argument
-# something that quacks like a hash can be used to set
-# extended properties on the server.
 server 'www2257ue.sakura.ne.jp', user: 'shun', roles: %w{web app db}
 
 # you can set custom ssh options
