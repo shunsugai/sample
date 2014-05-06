d50d7c4164cee37194200126334cd3c14359b230
Shun Sugai <sugaishun@gmail.com>
2014-05-06 17:53:41 +0900

    Fix unicorn's listen port

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,6 +1,6 @@
 project_root_dir = File.expand_path('../../../../', __FILE__)
 worker_processes 2
-listen      8080
+listen      '/tmp/sample-rails-app.sock'
 pid         "#{project_root_dir}/shared/tmp/pids/unicorn.pid"
 stdout_path "#{project_root_dir}/shared/log/production.log"
 stderr_path "#{project_root_dir}/shared/log/production.log"
